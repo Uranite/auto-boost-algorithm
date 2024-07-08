4a9ec1a9dd8f607d4b56da5b8b1459aa5cb93d93
Uranite <62639703+Uranite@users.noreply.github.com>
2024-07-08 19:30:12 +0700

    add crf granularity
    and fix mystery spaces
    
    idk what im doing

--- auto-boost_2.0.py
+++ auto-boost_2.0.py
@@ -14,13 +14,13 @@ if "--help" in sys.argv[1:]:
 else:
     pass
 
-og_cq = int(sys.argv[2]) # CQ to start from
+og_cq = float(sys.argv[2]) # CQ to start from
 br = 10 # maximum CQ change from original
 
 def get_ranges(scenes):
-     ranges = []
-     ranges.insert(0,0)
-     with open(scenes, "r") as file:
+    ranges = []
+    ranges.insert(0, 0)
+    with open(scenes, "r") as file:
         content = json.load(file)
         for i in range(len(content['scenes'])):
             ranges.append(content['scenes'][i]['end_frame'])
@@ -32,7 +32,7 @@ def zones_txt(beginning_frame, end_frame, cq, zones_loc):
     iter += 1
 
     with open(zones_loc, "w" if iter == 1 else "a") as file:
-        file.write(f"{beginning_frame} {end_frame} svt-av1 --crf {cq}\n")
+        file.write(f"{beginning_frame} {end_frame} svt-av1 --crf {cq:.2f}\n")
 
 def calculate_standard_deviation(score_list: list[int]):
     filtered_score_list = [score for score in score_list if score >= 0]
@@ -93,8 +93,7 @@ for i in range(len(ranges)-1):
 print(f'Median score:  {average}\n\n')
 
 for i in range(len(ranges)-1):
-
-    new_cq = og_cq - ceil((1.0 - (percentile_5_total[i]/average)) / 0.5 * 10) # trust me bro
+    new_cq = og_cq - ceil((1.0 - (percentile_5_total[i]/average)) / 0.5 * 40) / 4 # trust me bro
 
     if new_cq < og_cq-br: # set lowest allowed cq
         new_cq = og_cq-br
@@ -103,8 +102,8 @@ for i in range(len(ranges)-1):
         new_cq = og_cq+br
 
     print(f'Enc:  [{ranges[i]}:{ranges[i+1]}]\n'
-            f'Chunk 5th percentile: {percentile_5_total[i]}\n'
-            f'Adjusted CRF: {new_cq}\n\n')
+          f'Chunk 5th percentile: {percentile_5_total[i]}\n'
+          f'Adjusted CRF: {new_cq}\n\n')
     zones_txt(ranges[i], ranges[i+1], new_cq, f"{scenes_loc[:-11]}zones.txt")
 
 # yes, this is messier than the 1.0 code, laziness won over me, deal with it
